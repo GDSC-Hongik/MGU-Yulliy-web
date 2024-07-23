9ee4653b7b742e47bd12359793cb56543e6ab614
soulchicken <soulfever01@naver.com>
2024-07-23 15:39:18 +0900

    feat: 나눔 스퀘어 라운드 font import

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,5 +1,54 @@
+// 임시로 만들어본 폰트 테스트 페이지입니다.
+
+import styled, { css } from 'styled-components';
+
+interface TestDivProps {
+	fontWeight: 'regular' | 'bold' | 'extraBold' | 'light';
+}
+
 const HomePage = () => {
-	return <div>HomePage</div>;
+	return (
+		<Container>
+			<h1>Regular(400), Bold(700), Extra Bold(800), Light(300) 지원됩니다.</h1>
+			<br />
+			<TestDiv fontWeight="regular">폰트 테스트 1 : regular(400)</TestDiv>
+			<TestDiv fontWeight="bold">폰트 테스트 2 : bold(700)</TestDiv>
+			<TestDiv fontWeight="extraBold">폰트 테스트 3 : Extra Bold(800)</TestDiv>
+			<TestDiv fontWeight="light">폰트 테스트 4 :Light(300)</TestDiv>
+		</Container>
+	);
 };
 
 export default HomePage;
+
+const Container = styled.div`
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: center;
+	height: 100vh;
+`;
+
+const TestDiv = styled.div<TestDivProps>`
+	${({ fontWeight }) => fontWeightCSS(fontWeight)}
+`;
+
+// 폰트 가중치 스타일 정의
+const fontWeightCSS = (fontWeight: TestDivProps['fontWeight']) => css`
+	font-weight: ${getFontWeight(fontWeight)};
+`;
+
+const getFontWeight = (weight: TestDivProps['fontWeight']) => {
+	switch (weight) {
+		case 'bold':
+			return '700';
+		case 'regular':
+			return '400';
+		case 'light':
+			return '300';
+		case 'extraBold':
+			return '800';
+		default:
+			return '400';
+	}
+};
