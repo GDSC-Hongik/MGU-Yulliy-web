848e224415436d863e2bb125062457e45bd8ce0d
soulchicken <soulfever01@naver.com>
2024-07-23 15:02:13 +0900

    feat: 레이아웃, 테마, 컬러 세팅

---
+++ src/styles/GlobalStyles.tsx
@@ -0,0 +1,159 @@
+import { createGlobalStyle, css } from 'styled-components';
+import theme from './theme';
+
+const resetCss = css`
+	/* http://meyerweb.com/eric/tools/css/reset/ 
+   v2.0 | 20110126
+   License: none (public domain)
+*/
+
+	html,
+	body,
+	div,
+	span,
+	applet,
+	object,
+	iframe,
+	h1,
+	h2,
+	h3,
+	h4,
+	h5,
+	h6,
+	p,
+	blockquote,
+	pre,
+	a,
+	abbr,
+	acronym,
+	address,
+	big,
+	cite,
+	code,
+	del,
+	dfn,
+	em,
+	img,
+	ins,
+	kbd,
+	q,
+	s,
+	samp,
+	small,
+	strike,
+	strong,
+	sub,
+	sup,
+	tt,
+	var,
+	b,
+	u,
+	i,
+	center,
+	dl,
+	dt,
+	dd,
+	ol,
+	ul,
+	li,
+	fieldset,
+	form,
+	label,
+	legend,
+	table,
+	caption,
+	tbody,
+	tfoot,
+	thead,
+	tr,
+	th,
+	td,
+	article,
+	aside,
+	canvas,
+	details,
+	embed,
+	figure,
+	figcaption,
+	footer,
+	header,
+	hgroup,
+	menu,
+	nav,
+	output,
+	ruby,
+	section,
+	summary,
+	time,
+	mark,
+	audio,
+	video {
+		margin: 0;
+		padding: 0;
+		border: 0;
+		font-size: 100%;
+		font: inherit;
+		vertical-align: baseline;
+	}
+
+	/* HTML5 display-role reset for older browsers */
+	article,
+	aside,
+	details,
+	figcaption,
+	figure,
+	footer,
+	header,
+	hgroup,
+	menu,
+	nav,
+	section {
+		display: block;
+	}
+	body {
+		line-height: 1;
+	}
+	ol,
+	ul {
+		list-style: none;
+	}
+	blockquote,
+	q {
+		quotes: none;
+	}
+	blockquote:before,
+	blockquote:after,
+	q:before,
+	q:after {
+		content: '';
+		content: none;
+	}
+	table {
+		border-collapse: collapse;
+		border-spacing: 0;
+	}
+`;
+
+const GlobalStyles = createGlobalStyle`
+	${resetCss};
+
+	:root {
+		font-family: 'Nanum Gothic', sans-serif;
+		color: ${theme.colors.black};
+		background-color: ${theme.colors.whitegray};
+	}
+
+	* {
+    box-sizing: border-box !important;
+    margin: 0;
+    padding: 0;
+
+    font-family: inherit;
+    word-break: keep-all;
+    word-wrap: break-word;
+
+    -webkit-tap-highlight-color: transparent;
+  }
+`;
+
+export default GlobalStyles;
