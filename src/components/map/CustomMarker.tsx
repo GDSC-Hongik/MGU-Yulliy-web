e087cad6f3640df267f8c537187aa56c305ddd49
soulchicken <soulfever01@naver.com>
2024-07-26 17:28:56 +0900

    refactor: 커스텀 마커 컴포넌트화

---
+++ src/components/map/CustomMarker.tsx
@@ -0,0 +1,19 @@
+import { Marker } from 'react-naver-maps';
+import pinImage from '~/assets/pin.svg';
+
+type CustomMarkerProps = {
+	navermaps: typeof naver.maps;
+	lat: number;
+	lng: number;
+};
+
+const CustomMarker: React.FC<CustomMarkerProps> = ({ navermaps, lat, lng }) => {
+	return (
+		<Marker
+			position={new navermaps.LatLng(lat, lng)}
+			icon={{ url: pinImage }}
+		/>
+	);
+};
+
+export default CustomMarker;
