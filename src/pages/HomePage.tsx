71a1fde52cdfa3439a2ddab2f7b6d7869b0d9430
leejuhi <163210518+leejuhi@users.noreply.github.com>
2024-07-30 09:57:19 +0900

    feature: 메인 페이지 접근 제한
    로그인을 해야지 들어갈 수 있게 제한을 걸었습니당

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,8 +1,29 @@
 import styled from 'styled-components';
 import Map from '../components/Map';
 import SearchBar from '~/components/map/SearchBar';
+// 임시로 만들어본 폰트 테스트 페이지입니다.
+import { useEffect } from 'react';
+import axios from '../libs/axios';
+import { useNavigate } from 'react-router-dom';
+interface TestDivProps {
+	fontWeight: 'regular' | 'bold' | 'extraBold' | 'light';
+}
 
 const HomePage = () => {
+	const navigate = useNavigate();
+	useEffect(() => {
+		async function checkUser() {
+			try {
+				const res = await axios.get('/users/me');
+				if (res.data) {
+					navigate('/');
+				}
+			} catch (error) {
+				navigate('/login');
+			}
+		}
+		checkUser();
+	}, []);
 	return (
 		<Container>
 			<SearchBar />
