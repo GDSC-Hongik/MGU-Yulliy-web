98d745a859776d1200ba5acda8ef660d0360cc75
soulchicken <soulfever01@naver.com>
2024-07-30 15:45:47 +0900

    feat: searchBar에 아이콘 추가

---
+++ src/assets/icons/XIcon.tsx
@@ -0,0 +1,21 @@
+const XIcon = () => {
+	return (
+		<svg
+			width="10"
+			height="10"
+			viewBox="0 0 10 10"
+			fill="none"
+			xmlns="http://www.w3.org/2000/svg"
+		>
+			<path
+				d="M9 1L1 9M1 1L9 9"
+				stroke="#60646C"
+				stroke-width="1.6"
+				stroke-linecap="round"
+				stroke-linejoin="round"
+			/>
+		</svg>
+	);
+};
+
+export default XIcon;
