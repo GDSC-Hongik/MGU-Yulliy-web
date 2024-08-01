04ee90126c1381ed6ba134ab533270c010b43725
soulchicken <soulfever01@naver.com>
2024-08-01 15:14:53 +0900

    feat: bottom sheet 마우스 드래그

--- src/components/bottomSheet/BottomSheet.tsx
+++ src/components/bottomSheet/BottomSheet.tsx
@@ -1,26 +1,46 @@
+import { useCallback, useEffect, useState } from 'react';
 import styled from 'styled-components';
 
 type BottomSheetProps = {
 	onClose: () => void;
 };
 
-const BottomSheetWrapper = styled.div`
+const calculateInitialTranslateY = () => {
+	const vh = window.innerHeight;
+	const initialHeight = 400;
+	return vh - initialHeight;
+};
+
+const BottomSheetWrapper = styled.div<{ translateY: number }>`
 	position: fixed;
 	bottom: 0;
-	left: 0;
-	right: 0;
-	background-color: white;
-	box-shadow: 0 -2px 10px rgba(0, 0, 0, 0.1);
+	z-index: 100;
+	width: 400px;
+	background-color: ${({ theme }) => theme.colors.white};
+	filter: drop-shadow(0 4px 20px rgba(0, 0, 0, 0.2));
+
 	border-top-left-radius: 16px;
 	border-top-right-radius: 16px;
-	max-height: 50vh;
+
+	max-height: 80vh;
+
 	overflow-y: auto;
-	transition: transform 0.3s ease-in-out;
-	transform: translateY(0);
+	transform: translateY(${({ translateY }) => translateY}px);
+`;
+
+const Handle = styled.div`
+	width: 50px;
+	height: 5px;
+	background-color: ${({ theme }) => theme.colors.gray};
+	border-radius: 5px;
+	margin: 10px auto;
+	cursor: grab;
 `;
 
 const BottomSheetContent = styled.div`
 	padding: 16px;
+	// TODO: 임시 크기
+	height: 1000px;
 `;
 
 const CloseButton = styled.button`
@@ -28,10 +48,55 @@ const CloseButton = styled.button`
 `;
 
 const BottomSheet: React.FC<BottomSheetProps> = ({ onClose }) => {
+	const [isDragging, setIsDragging] = useState(false);
+	const [startY, setStartY] = useState(0);
+	const [translateY, setTranslateY] = useState(calculateInitialTranslateY());
+
+	const handleMouseDown = (event: React.MouseEvent) => {
+		setIsDragging(true);
+		setStartY(event.clientY);
+	};
+
+	// 드래그 진행 중
+	const handleMouseMove = useCallback(
+		(event: MouseEvent) => {
+			if (!isDragging) return;
+			const offsetY = event.clientY - startY;
+			setTranslateY((prev) => Math.max(0, prev + offsetY));
+			setStartY(event.clientY);
+		},
+		[isDragging, startY],
+	);
+
+	// 드래그 종료
+	const handleMouseUp = useCallback(() => {
+		setIsDragging(false);
+		if (window.innerHeight - startY < 70) {
+			onClose();
+		}
+	}, [startY, onClose]);
+
+	useEffect(() => {
+		if (isDragging) {
+			window.addEventListener('mousemove', handleMouseMove);
+			window.addEventListener('mouseup', handleMouseUp);
+		} else {
+			window.removeEventListener('mousemove', handleMouseMove);
+			window.removeEventListener('mouseup', handleMouseUp);
+		}
+
+		return () => {
+			window.removeEventListener('mousemove', handleMouseMove);
+			window.removeEventListener('mouseup', handleMouseUp);
+		};
+	}, [isDragging, handleMouseMove, handleMouseUp]);
+
 	return (
-		<BottomSheetWrapper>
+		<BottomSheetWrapper translateY={translateY}>
+			<Handle onMouseDown={handleMouseDown} />
 			<BottomSheetContent>
 				<h2>바텀 시트</h2>
+				<p>내용</p>
 				<CloseButton onClick={onClose}>닫기</CloseButton>
 			</BottomSheetContent>
 		</BottomSheetWrapper>
