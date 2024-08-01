bff92545beb6242275da64c2a26865392072b858
soulchicken <soulfever01@naver.com>
2024-08-01 13:58:28 +0900

    feat: bottom sheet handling

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,10 +1,11 @@
 import styled from 'styled-components';
 import Map from '../components/Map';
 import SearchBar from '~/components/map/SearchBar';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import axios from '../libs/axios';
 import { useNavigate } from 'react-router-dom';
 import NavBar from '~/components/navBar/NavBar';
+import BottomSheet from '~/components/bottomSheet/BottomSheet';
 
 const HomePage = () => {
 	const navigate = useNavigate();
@@ -21,15 +22,27 @@ const HomePage = () => {
 		}
 		checkUser();
 	}, [navigate]);
+
+	const [isBottomSheetVisible, setIsBottomSheetVisible] = useState(false);
+
+	const handleMapClick = () => {
+		setIsBottomSheetVisible(true);
+	};
+
+	const handleCloseBottomSheet = () => {
+		setIsBottomSheetVisible(false);
+	};
+
 	return (
 		<>
 			<Container>
 				<SearchBar />
 				<MapWrapper>
-					<Map />
+					<Map onClick={handleMapClick} />
 				</MapWrapper>
 			</Container>
-			<NavBar />
+			{isBottomSheetVisible && <BottomSheet onClose={handleCloseBottomSheet} />}
+			{!isBottomSheetVisible && <NavBar />}
 		</>
 	);
 };
