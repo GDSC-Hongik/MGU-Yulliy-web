eba0fcb03b2c902ee3c61db5f762f722da16f84a
soulchicken <soulfever01@naver.com>
2024-08-02 17:36:28 +0900

    feat: jotai로 레스토랑 데이터 전역 상태 지정

--- src/hooks/api/useGetRestaurants.ts
+++ src/hooks/api/useGetRestaurants.ts
@@ -1,8 +1,11 @@
 import { useQuery } from '@tanstack/react-query';
 import { get } from '~/libs/api';
+import { UserRestaurant } from '~/types/restaurants';
+
+type Response = UserRestaurant[];
 
 const useGetRestaurants = () => {
-	return useQuery({
+	return useQuery<Response>({
 		queryKey: ['restaurants'],
 		queryFn: () => get('/restaurants'),
 	});
