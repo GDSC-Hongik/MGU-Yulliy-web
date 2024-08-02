73987a3bf1d8c97f1ad36f439759348aab684e01
soulchicken <soulfever01@naver.com>
2024-08-02 23:41:59 +0900

    feat: 레스토랑 더보기 버튼

---
+++ src/components/bottomSheet/restaurantSummary/MoreButton.tsx
@@ -0,0 +1,46 @@
+import { styled } from 'styled-components';
+import MoreIcon from '~/assets/icons/MoreIcon';
+
+const Button = styled.button`
+	flex-shrink: 0;
+	width: 50px;
+	height: 50px;
+	background-color: ${({ theme }) => theme.colors.orange};
+	color: ${({ theme }) => theme.colors.white};
+	border: none;
+	font-size: 10px;
+	font-weight: ${({ theme }) => theme.fonts.Bold};
+	text-align: center;
+	border-radius: 50%;
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+	gap: 8px;
+	cursor: pointer;
+
+	&:hover {
+		background-color: ${({ theme }) => theme.colors.grayorange};
+		color: ${({ theme }) => theme.colors.white};
+
+		& svg > path {
+			fill: ${({ theme }) => theme.colors.white};
+		}
+	}
+`;
+
+const MoreButton = () => {
+	const handleClick = () => {
+		// TODO: 더보기 버튼 클릭 시 상세 정보 표시
+		console.log('MoreButton');
+	};
+
+	return (
+		<Button onClick={handleClick}>
+			<MoreIcon />
+			MORE
+		</Button>
+	);
+};
+
+export default MoreButton;
