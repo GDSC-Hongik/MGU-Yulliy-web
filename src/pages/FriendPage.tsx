46631f444a292cd79371631a247d0395b3c51a82
leejuhi <163210518+leejuhi@users.noreply.github.com>
2024-08-04 06:54:57 +0900

    feat: 친구목록 기능 추가
    api가 없어서 아직 확인 할 수가 없네용

--- src/pages/FriendPage.tsx
+++ src/pages/FriendPage.tsx
@@ -1,11 +1,20 @@
 import styled from 'styled-components';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import axios from '../libs/axios';
 import { useNavigate } from 'react-router-dom';
 import NavBar from '~/components/navBar/NavBar';
 
+interface Friend {
+	id: number;
+	profile: string;
+	name: string;
+	email: string;
+}
+
 const FriendPage = () => {
 	const navigate = useNavigate();
+	const [friends, setFriends] = useState<Friend[]>([]);
+
 	useEffect(() => {
 		async function checkUser() {
 			try {
@@ -19,9 +28,26 @@ const FriendPage = () => {
 		}
 		checkUser();
 	}, [navigate]);
+
+	useEffect(() => {
+		async function fetchFriends() {
+			const response = await axios.get('/friends');
+			setFriends(response.data);
+		}
+
+		fetchFriends();
+	}, []);
+
 	return (
 		<>
-			<Container></Container>
+			<Title> friends</Title>
+			<FriendList>
+				{friends.map((friend) => (
+					<FriendItem key={friend.id}>
+						{friend.profile} {friend.name}
+					</FriendItem>
+				))}
+			</FriendList>
 			<NavBar />
 		</>
 	);
@@ -29,9 +55,29 @@ const FriendPage = () => {
 
 export default FriendPage;
 
-const Container = styled.div`
-	display: flex;
-	flex-direction: column;
-	align-items: center;
-	justify-content: center;
+// 스타일링
+
+const Title = styled.p`
+	margin: 20px 0;
+	font-size: 24px;
+	text-align:left;
+	box-sizing:border-box:
+	width:350px;
+	height:100%;
+	padding:20px;
+`;
+
+const FriendList = styled.ul`
+	list-style: none;
+	padding: 0;
+	margin: 0;
+	width: 100%;
+	max-width: 600px;
+`;
+
+const FriendItem = styled.li`
+	padding: 10px;
+	border-bottom: 1px solid #ccc;
+	width: 100%;
+	text-align: center;
 `;
