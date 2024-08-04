12dfb67d0ed15137d08a810edd4be525e285b4e3
soulchicken <soulfever01@naver.com>
2024-08-04 15:11:30 +0900

    feat: 레스토랑 데이터 API 폼 변경 반영

--- src/components/bottomSheet/restaurantSummary/RestaurantSummary.tsx
+++ src/components/bottomSheet/restaurantSummary/RestaurantSummary.tsx
@@ -47,23 +47,6 @@ const Category = styled.p`
 const RestaurantSummary: React.FC<RestaurantSummaryProps> = ({
 	restaurant,
 }) => {
-	const calculateAverageRating = (): number => {
-		const { rating_naver, rating_kakao, rating_google } = restaurant;
-		const ratings = [
-			parseFloat(rating_naver || '0'),
-			parseFloat(rating_kakao || '0'),
-			parseFloat(rating_google || '0'),
-		];
-		// 유효한 평점 수 계산
-		const validRatings = ratings.filter((rating) => rating > 0);
-		// 유효한 평점이 없으면 'No ratings available' 반환
-		if (validRatings.length === 0) return 0;
-		// 평균 계산
-		const average =
-			validRatings.reduce((acc, curr) => acc + curr, 0) / validRatings.length;
-		return Number(average.toFixed(2));
-	};
-
 	return (
 		<SummaryWrapper>
 			<RestaurantImgBox />
@@ -71,7 +54,7 @@ const RestaurantSummary: React.FC<RestaurantSummaryProps> = ({
 				<Title>{restaurant.name}</Title>
 				{/* TODO: address가 아니라 카테고리로 변경 */}
 				<Category>{restaurant.address}</Category>
-				<StarRating rating={calculateAverageRating()} />
+				<StarRating rating={restaurant.rating_average} />
 			</SummaryInfo>
 			<MoreButton />
 		</SummaryWrapper>
