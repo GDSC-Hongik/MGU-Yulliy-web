76d2734447ba96fa00f3c033026a65d52cbbfae2
soulchicken <soulfever01@naver.com>
2024-08-15 00:42:26 +0900

    feat: 식당 디테일을 보면 지도 센터에 식당으로 panTo

--- src/components/map/CustomMarker.tsx
+++ src/components/map/CustomMarker.tsx
@@ -1,17 +1,24 @@
 import { Marker } from 'react-naver-maps';
 import pinImage from '~/assets/images/pin.svg';
+import greenPinImage from '~/assets/images/greenPin.svg';
 
 type CustomMarkerProps = {
 	navermaps: typeof naver.maps;
 	lat: number;
 	lng: number;
+	isCenter: boolean;
 };
 
-const CustomMarker: React.FC<CustomMarkerProps> = ({ navermaps, lat, lng }) => {
+const CustomMarker: React.FC<CustomMarkerProps> = ({
+	navermaps,
+	lat,
+	lng,
+	isCenter,
+}) => {
 	return (
 		<Marker
 			position={new navermaps.LatLng(lat, lng)}
-			icon={{ url: pinImage }}
+			icon={{ url: isCenter ? greenPinImage : pinImage }}
 		/>
 	);
 };
--- src/store/geoLocates.ts
+++ src/store/geoLocates.ts
@@ -6,3 +6,7 @@ export const geoLocationAtom = atom<GeoLocation>({
 	longitude: 126.9203867,
 });
 export const geoLocationErrorAtom = atom<string | null>(null);
+
+export const mapAtom = atom<null | naver.maps.Map>(null);
+
+export const mapCenterAtom = atom<GeoLocation | null>(null);
